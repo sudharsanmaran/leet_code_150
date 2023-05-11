4a431f1914aa607e452b47b07fa17794cb536df7
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-05-11 16:38:26 +0530

    jgbv

--- arrays_hashing/topKFrequent.py
+++ arrays_hashing/topKFrequent.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def top_k_frequent(nums: List[int], k: int) -> List[int]:
-    """t O(n * k * log(k)) / s O(n)"""
+    """t O(n * n * log(n)) / s O(n)"""
     from collections import Counter
     counter = Counter(nums)
     return [num for num, _ in counter.most_common(k)]
@@ -16,5 +16,22 @@ def top_k_frequent_1(nums: List[int], k: int) -> List[int]:
     return [num for num, _ in sorted(counter.items(), key=lambda x: x[1], reverse=True)[:k]]
 
 
+def top_k_frequent_2(nums: List[int], k: int) -> List[int]:
+    """used heap instead of sorting"""
+    """t O(n * k log(k)) / s O(n + k)"""
+    counter = {}
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+    import heapq
+    heap = []
+    for num, count in counter.items():
+        if len(heap) < k:
+            heapq.heappush(heap, (count, num))
+        else:
+            heapq.heappushpop(heap, (count, num))
+
+    return [num for count, num in heap]
+
+
 if __name__ == '__main__':
-    print(top_k_frequent_1(nums=[1, 1, 1, 2, 2, 3], k=2))
+    print(top_k_frequent_2(nums=[1, 1, 1, 2, 2, 3, 5, 5], k=2))
--- arrays_hashing/two_sum.py
+++ arrays_hashing/two_sum.py
@@ -14,5 +14,17 @@ def two_sum(nums: List[int], target: int) -> List[int]:
     return
 
 
+def two_sum_1(nums: List[int], target: int) -> List[int]:
+    """t O(nums) / s O(nums)"""
+    counter = {}
+    for idx, num in enumerate(nums):
+        to_find = target - num
+        if to_find in counter:
+            return [idx, counter.get(to_find)]
+        else:
+            counter[num] = idx
+    return
+
+
 if __name__ == '__main__':
-    print(two_sum([-1,-2,-3,-4,-5], -8))
+    print(two_sum_1([-1, -2, -3, -4, -5], -8))
