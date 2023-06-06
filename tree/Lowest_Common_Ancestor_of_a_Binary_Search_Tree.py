d3bb86ef051e8e87574a550657c978f2572795ff
sudharsanmaran <smsudharsanmaran@gmail.com>
2023-06-06 11:30:30 +0530

    tree LCU

--- tree/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ tree/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -1,17 +1,47 @@
+from collections import deque
+
 from tree.Invert_Binary_Tree import TreeNode
+
+"""approach:
+1. traverse binary tree until we find the split of two given nodes. t => O(log n)
+1.1 when ever the split occur that's our least common ancestor
+
+edge case:
+1. what if root node itself one of the given two nodes
+    => then root node must be LCA
+"""
 
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        if p.val <= root.val:
-            self.lowestCommonAncestor(root.left, p, q)
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                return root.val
 
     def bfs(self, root):
-        pass
+        if not root:
+            return []
+        node_values = []
+        total_sum = 0
+        queue = deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            node_values.append(node.val)
+            total_sum += node.val
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return node_values
 
 
 if __name__ == '__main__':
-    # Create two identical binary trees for testing
     # Tree:
     #       6
     #    /     \
@@ -31,4 +61,4 @@ if __name__ == '__main__':
     tree.right.right = TreeNode(9)
 
     solution = Solution()
-    print(solution.lowestCommonAncestor(tree))
+    print(solution.lowestCommonAncestor(tree, tree.right.left, tree.right.right))
